7ea8af540ffdeb9b704e9e13ebdeb9c4f8342719
Khamhuang, David <khameye19888@gmail.com>
2019-08-25 22:56:57 -0500

    added more features

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection ({
     host: "localhost",
     port: 3000,
-    user: "root",
-    password: "chocolateMint",
+    user: "",
+    password: "",
     database: "burgers_db"
 });
 
