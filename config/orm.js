08a843b1017e8fd3df84936fdef76ebf0d86a345
Khamhuang, David <khameye19888@gmail.com>
2019-08-27 11:50:52 -0500

    added function to ORM

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,28 @@
 // Importing the ./connection.js to make connection to MySQL
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+
+function objToSql(ob) {
+    // column1=value, column2=value2
+    var arr = [];
+
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+
+    return arr.toString();
+}
+
 var orm = {
     all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
@@ -9,7 +31,14 @@ var orm = {
             if (err) {
                 throw err;
             }
+            cb(result);
         });
     },
-    
+// vals is an array of values that we want to save to cols
+// cols are the columns we want to insert the values into
+
+create: function(table, cols, vals, cb) {
+    var queryString + "INSERT INTO " + table;
+
+}
 }
